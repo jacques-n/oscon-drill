495c420d423141703e08bcb17a2b8239ecf498dc
Jacques Nadeau <jacques@apache.org>
2013-07-23 12:36:56 -0700

    Add SinFunction example using Float8

--- compile/src/main/java/org/apache/drill/oscon/compile/SinFunction.java
+++ compile/src/main/java/org/apache/drill/oscon/compile/SinFunction.java
@@ -4,17 +4,20 @@ import org.apache.drill.common.expression.ArgumentValidators;
 import org.apache.drill.common.expression.CallProvider;
 import org.apache.drill.common.expression.FunctionDefinition;
 import org.apache.drill.common.expression.OutputTypeDeterminer;
+import org.apache.drill.common.expression.OutputTypeDeterminer.FixedType;
+import org.apache.drill.common.types.TypeProtos.MinorType;
+import org.apache.drill.common.types.Types;
 import org.apache.drill.exec.expr.DrillFunc;
 import org.apache.drill.exec.expr.annotations.FunctionTemplate;
 import org.apache.drill.exec.expr.annotations.FunctionTemplate.FunctionScope;
 import org.apache.drill.exec.expr.annotations.FunctionTemplate.NullHandling;
 import org.apache.drill.exec.expr.annotations.Output;
 import org.apache.drill.exec.expr.annotations.Param;
-import org.apache.drill.exec.expr.holders.LongHolder;
+import org.apache.drill.exec.expr.holders.Float8Holder;
 import org.apache.drill.exec.record.RecordBatch;
 
-public class AbsoluteFunction {
-  static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(AbsoluteFunction.class);
+public class SinFunction {
+  static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SinFunction.class);
   
   /**
    * Inform drill about the existence of the Function Definition via class path scanning.
@@ -23,8 +26,8 @@ public class AbsoluteFunction {
     @Override
     public FunctionDefinition[] getFunctionDefintions() {
       return new FunctionDefinition[]{
-          FunctionDefinition.operator("abs", new ArgumentValidators.NumericTypeAllowed(1, 2, true), //
-              new OutputTypeDeterminer.SameAsAnySoft(), "abs")
+          FunctionDefinition.operator("sin", new ArgumentValidators.NumericTypeAllowed(1, 2, true), // 
+              new OutputTypeDeterminer.SameAsAnySoft(), "sin")
       };
     }
   }
@@ -32,16 +35,16 @@ public class AbsoluteFunction {
   /** 
    * Define the actual absolute value implementation
    */
-  @FunctionTemplate(name = "abs", scope = FunctionScope.SIMPLE, nulls = NullHandling.NULL_IF_NULL)
+  @FunctionTemplate(name = "sin", scope = FunctionScope.SIMPLE, nulls = NullHandling.NULL_IF_NULL)
   public static class AbsoluteImpl implements DrillFunc{
     
-    @Param LongHolder input;
-    @Output LongHolder out;
+    @Param Float8Holder input;
+    @Output Float8Holder out;
 
     public void setup(RecordBatch b){}
     
     public void eval(){
-      out.value = Math.abs(input.value);
+      out.value = Math.sin(input.value);
     }
 
   }
